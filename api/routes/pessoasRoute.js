c6580b12b9c7dd39d11a9342eb68b4574e98f3cc
Leandro Simeão <leandrosimeao@yahoo.com.br>
2022-06-29 18:11:11 -0300

    Implementando método get

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-const { pegaTodasAsPessoas } = require('../controllers/PessoaController');
 const PessoaController = require('../controllers/PessoaController');
 
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
 module.exports = router;
